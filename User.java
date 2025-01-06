3dbc0db09d3888e9a72e1202de601b7918f4640c
barenzel <bar.enzel@post.runi.ac.il>
2025-01-06 09:38:24 +0200

    done

--- User.java
+++ User.java
@@ -53,17 +53,17 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (fCount >= maxfCount) {
+        if (fCount >= maxfCount || this.follows(name)) {
             return false;
         }
         for (int i = 0; i < fCount; i++) {
-            if (this.follows[i] != null && this.follows[i].equals(name)) {
-                return false;
+            if (this.follows[i] == null) {
+                this.follows[fCount] = name;
+                fCount++;
+                return true;
             }
         }
-        this.follows[fCount] = name;
-        fCount++;
-        return true;
+        return false;
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
