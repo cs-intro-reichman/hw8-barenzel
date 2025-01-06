1929434f609312efb1eba91d162800ea8e23fa9c
barenzel <bar.enzel@post.runi.ac.il>
2025-01-06 17:43:58 +0200

    done

--- Network.java
+++ Network.java
@@ -116,9 +116,10 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
         String ans = "Network: \n";
-        for (int i = 0; i < userCount; i++) {
-            ans = ans + users[i].toString() + "\n";
+        for (int i = 0; i < userCount - 1; i++) {
+            ans += users[i].toString() + "\n";
         }
+        ans += users[userCount - 1];
         return ans;
     }
 }
