329c6df49ce07187af8aa742807388151c273cb8
barenzel <bar.enzel@post.runi.ac.il>
2025-01-07 22:16:21 +0200

    done

--- Network.java
+++ Network.java
@@ -46,17 +46,20 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        if (userCount >= users.length) {
+        if (name == null){
             return false;
         }
-        for (int i = 0; i < userCount; i++) {
-            if (this.users[i] != null && this.users[i].getName().equals(name)) {
-                return false;
+        if (this.getUser(name) != null){
+            return false;
+        }
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] == null){
+                users[i]= new User(name);
+                userCount++;
+                return true;
             }
         }
-        this.users[userCount] = new User(name);
-        userCount++;
-        return true;
+        return false;
     }
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
